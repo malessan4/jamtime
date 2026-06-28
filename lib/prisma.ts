77aa93185852ded9784382852bc26ec683aedd98
malessan4 <matialess@gmail.com>
2026-06-28 16:45:40 -0300

    ajustamos para que se inyecte bien el .env en docker

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -5,22 +5,27 @@ import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '@prisma/client';
 
 const prismaClientSingleton = () => {
-    // 1. Inicializamos el Pool de conexiones nativo con la URL de Supabase
-    const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+  // 1. Limpiamos cualquier comilla que Docker haya inyectado por accidente
+  const cleanUrl = process.env.DATABASE_URL?.replace(/['"]/g, '');
 
-    // 2. Envolvemos el pool con el adaptador oficial de Prisma para Postgres
-    const adapter = new PrismaPg(pool);
-
-    // 3. Prisma 7 requiere que le pases el adapter en lugar de datasources
-    return new PrismaClient({ adapter });
+  // 2. Inicializamos el Pool. 
+  const pool = new Pool({ 
+    connectionString: cleanUrl 
+  });
+  
+    // 3. Envolvemos el pool con el adaptador oficial de Prisma para Postgres 
+  const adapter = new PrismaPg(pool);
+  
+    // 4. Prisma 7 requiere que le pases el adapter en lugar de datasources
+  return new PrismaClient({ adapter });
 };
 
 declare global {
-    var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>;
+  var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>;
 }
 
 const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
 
 export default prisma;
 
-if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma;
+if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma;3
